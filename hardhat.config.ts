330cbc28d6a7948e4b7b036210c3362eab93c6ce
dzgoldman <dzgoldman@wesleyan.edu>
2022-09-16 10:03:33 -0400

    first commit; hardhat stuff

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9"
+      }
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
+
+export default config;
